8e59c9f74612b2f706425b9d5b9588d013b91bca
angelobguido <angelobguido@gmail.com>
2024-06-20 00:14:47 -0300

    Create first Triangle

---
+++ compile.sh
@@ -0,0 +1,2 @@
+glslc ./shaders/app.vert -o ./shaders/vert.spv
+glslc ./shaders/app.frag -o ./shaders/frag.spv
--- source/HelloTriangleApplication.h
+++ source/HelloTriangleApplication.h
@@ -8,6 +8,9 @@
 #include <cstring>
 #include <vector>
 #include <optional>
+#include <set>
+#include <algorithm>
+#include <fstream>
 
 namespace app {
 
@@ -18,6 +21,10 @@ namespace app {
             "VK_LAYER_KHRONOS_validation"
     };
 
+    const std::vector<const char*> deviceExtensions = {
+            VK_KHR_SWAPCHAIN_EXTENSION_NAME
+    };
+
     #ifdef NDEBUG
         const bool enableValidationLayers = false;
     #else
@@ -26,17 +33,35 @@ namespace app {
 
     struct QueueFamilyIndices {
          std::optional<uint32_t> graphicsFamily;
+         std::optional<uint32_t> presentFamily;
 
          bool isComplete() {
-             return graphicsFamily.has_value();
+             return graphicsFamily.has_value() && presentFamily.has_value();
          }
     };
 
-    QueueFamilyIndices findQueueFamilies(VkPhysicalDevice device);
+    struct SwapChainSupportDetails {
+        VkSurfaceCapabilitiesKHR capabilities;
+        std::vector<VkSurfaceFormatKHR> formats;
+        std::vector<VkPresentModeKHR> presentModes;
+    };
 
-    bool isDeviceSuitable(VkPhysicalDevice device);
+    SwapChainSupportDetails querySwapChainSupport(VkPhysicalDevice device, VkSurfaceKHR surface);
+
+    QueueFamilyIndices findQueueFamilies(VkPhysicalDevice device, VkSurfaceKHR surface);
+
+    bool isDeviceSuitable(VkPhysicalDevice device, VkSurfaceKHR surface);
+
+    bool checkDeviceExtensionSupport(VkPhysicalDevice device);
 
     bool checkValidationLayerSupport();
+
+    VkSurfaceFormatKHR chooseSwapSurfaceFormat(const std::vector<VkSurfaceFormatKHR>& availableFormats);
+    VkPresentModeKHR chooseSwapPresentMode(const std::vector<VkPresentModeKHR>& availablePresentModes);
+    VkExtent2D chooseSwapExtent(const VkSurfaceCapabilitiesKHR& capabilities, GLFWwindow* window);
+
+    std::vector<char> readFile(const std::string& filename);
+    VkShaderModule createShaderModule(const std::vector<char>& code, VkDevice device);
 
     class HelloTriangleApplication {
 
@@ -46,6 +71,22 @@ namespace app {
         VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
         VkDevice device;
         VkQueue graphicsQueue;
+        VkQueue presentQueue;
+        VkSurfaceKHR surface;
+        VkSwapchainKHR swapChain;
+        std::vector<VkImage> swapChainImages;
+        VkFormat swapChainImageFormat;
+        VkExtent2D swapChainExtent;
+        std::vector<VkImageView> swapChainImageViews;
+        VkRenderPass renderPass;
+        VkPipelineLayout pipelineLayout;
+        VkPipeline graphicsPipeline;
+        std::vector<VkFramebuffer> swapChainFramebuffers;
+        VkCommandPool commandPool;
+        VkCommandBuffer commandBuffer;
+        VkSemaphore imageAvailableSemaphore;
+        VkSemaphore renderFinishedSemaphore;
+        VkFence inFlightFence;
 
     public:
         void run();
@@ -56,15 +97,37 @@ namespace app {
 
         void initVulkan();
 
-        void createInstance();
-
         void mainLoop();
 
         void cleanup();
 
+        void createInstance();
+
         void pickPhysicalDevice();
 
         void createLogicalDevice();
+
+        void crateSurface();
+
+        void createSwapChain();
+
+        void createImageViews();
+
+        void createGraphicsPipeline();
+
+        void createRenderPass();
+
+        void createFramebuffers();
+
+        void createCommandPool();
+
+        void createCommandBuffer();
+
+        void recordCommandBuffer(VkCommandBuffer commandBuffer, uint32_t imageIndex);
+
+        void drawFrame();
+
+        void createSyncObjects();
 
     };
 
